392698281412999e7bfdc5099781d13e1648035a
byongkicho <weaponcho@gmail.com>
2024-02-19 20:23:05 +0900

    깃 저장소 초기화

---
+++ README.md
@@ -0,0 +1,2 @@
+# longi- lecture
+백엔트 맞보기 강의
---
+++ bin/www.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const app = require('../app');
+const PORT = 43299;
+
+app.listen( PORT, () => {
+    console.log(`${PORT}포트 서버 가동`)
+});
