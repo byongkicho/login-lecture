7b4e5551e349289244e6ec48be1b8913e14423e9
byongkicho <weaponcho@gmail.com>
2024-02-20 13:10:45 +0900

    DOM을 이용하여 HTML객체저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,20 @@
 'use strict';
-console.log('Hello yo');
-console.log('bye');
+//  DOM ==> document Object Models
+
+const id = document.querySelector('#id');
+const psword = document.querySelector('#psword');
+const loginBtn = document.querySelector('button');
+
+console.log(id.outerHTML);
+console.log(psword.outerHTML);
+console.log(loginBtn.outerHTML);
+
+loginBtn.addEventListener('click',login);
+
+function login () {
+    const req = {
+        id : id.value,
+        psword : psword.value,
+    };
+    console.log(req);
+};
