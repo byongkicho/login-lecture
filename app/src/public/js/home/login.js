751e8c920c67b9b65be2132cfd4796ab863cae7c
byongkicho <weaponcho@gmail.com>
2024-02-20 17:25:02 +0900

    로그인 결과 회신(성공,실패,에러)

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -24,13 +24,15 @@ function login () {
         },
         body : JSON.stringify(req),
     })
-        .then( (res) => res.json())
-        .then( (res) => console.log(res));
-    // fetch('/login', {
-    //     method: "POST",
-    //     headers : {
-    //         "Content-Type" : "application/json",
-    //     },
-    //     body : JSON.stringify(req),
-    // });
+    .then( (res) => res.json())
+    .then( (res) => {
+        if (res.success) {
+            location.href = "/";
+        } else {
+            alert(res.msg);
+        }
+    })
+    .catch( (err) => {
+        console.error( "로그인 중 에러 발생");
+    });
 };
