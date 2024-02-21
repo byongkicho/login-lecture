d5db7e8c8c1a39e18a7f9da2b2347d003b48b56b
byongkicho <weaponcho@gmail.com>
2024-02-21 14:34:12 +0900

    medels 생성

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,15 +1,12 @@
 'use strict'
 
-const users = {
-    id : ["woorimIT", "나개발", "김팀장"],
-    psword :["1234", "1234", "123456"],
-};
+const UserStorage = require('../../models/UserStorage');
 
 const output = {
-     home : ( req, res ) => {
+    home : ( req, res ) => {
         res.render('home/index');
     },
-     login : (req, res) => {
+    login : (req, res) => {
         res.render('home/login')
     },
 }
@@ -18,21 +15,20 @@ const process = {
     login : (req, res) => {
         const id = req.body.id;
         const psword = req.body.psword;
-        
+        const users = UserStorage.getUsers('id','psword');
+        const response = {};
         if( users.id.includes(id)){
             const index = users.id.indexOf(id);
             if( users.psword[index] === psword) {
                 console.log("로그인 성공")
-                return res.json({
-                    success : true,
-                });
+                response.success = true;
+                return res.json(response);
             }
         }
-        console.log("로그인 실패")
-        return res.json({
-            success: false,
-            msg : "로그인에 실패 하셨습니다."
-        })
+        console.log("로그인 실패");
+        response.success = false,
+        response.msg = "로그인에 실패 하셨습니다."
+        return res.json(response);
     }
 }
 
